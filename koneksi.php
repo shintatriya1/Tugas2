cc5fb0dbb09183c32b058f39779d9645efbc4921
shintatriya1 <48477616+shintatriya1@users.noreply.github.com>
2019-03-19 15:55:08 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    
+    $kon = mysqli_connect("localhost", "id8951548_shinta",'triyashinta18',"id8951548_akademik");
+
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
